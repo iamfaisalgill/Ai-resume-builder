0e3d1f4eaceba6e3d85e07ebbd50665a94527539
faisal gill <faisalzakriagill@gmail.com>
2025-03-13 15:54:33 +0500

    Add utility functions, button component, and Tailwind CSS configuration

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from '../components/Header'
 import { Link } from 'react-router-dom'
+import { Button } from '@/components/ui/button'
 
 const Home = () => {
   return (
     <div>
     <Header/>
+    <Button>  
       <Link to='/resumebuild/contact'>Start</Link>
+    </Button>
     </div>
   )
 }
